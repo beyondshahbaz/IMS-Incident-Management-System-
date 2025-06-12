f18dbee56cd6688e8925241c779799f3be4637a6
beyondshahbaz <sahbajali187@gmail.com>
2025-06-12 14:44:59 +0530

    customize jwt token

--- IMS/serializers.py
+++ IMS/serializers.py
@@ -1,6 +1,20 @@
 from rest_framework import serializers
 from IMS.models import *
 from time import timezone
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['employee_id'] = user.employee.employee_id
+        # ...
+
+        return token
+
 
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
